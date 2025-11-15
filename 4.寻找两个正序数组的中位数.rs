c106eabf10193995fccb4c6762da8906d27817cc
3192575523 <q2367485914@163.com>
2025-11-15 10:05:07 +0800

    upload the 4th question

---
+++ 4.寻找两个正序数组的中位数.rs
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=rust
+ *
+ * [4] 寻找两个正序数组的中位数
+ *
+ * https://leetcode.cn/problems/median-of-two-sorted-arrays/description/
+ *
+ * algorithms
+ * Hard (44.00%)
+ * Likes:    7756
+ * Dislikes: 0
+ * Total Accepted:    1.4M
+ * Total Submissions: 3.2M
+ * Testcase Example:  '[1,3]\n[2]'
+ *
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * 
+ * 算法的时间复杂度应该为 O(log (m+n)) 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let mut merged: Vec<i32> = nums1.into_iter().chain(nums2.into_iter()).collect();
+        merged.sort_unstable();
+        let len = merged.len();
+        if len % 2 == 0 {
+            (merged[len / 2 - 1] as f64 + merged[len / 2] as f64) / 2.0
+        } else {
+            merged[len / 2] as f64
+        }
+    }
+}
+// @lc code=end
+
